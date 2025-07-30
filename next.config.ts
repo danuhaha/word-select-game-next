bb763042ce67964103d63901d2afb6647d5e7a1f
danuhaha <dgorinmail@mail.ru>
2025-07-30 14:57:24 +0300

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/word-select-game-next' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/word-select-game-next/' : '',
   images: {
     unoptimized: true,
   },
